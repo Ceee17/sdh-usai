3080e5b1e3cc54db1a189e95352b63dd7308d70c
Timot <tim7wp7@gmail.com>
2024-06-10 22:48:57 +0700

    add login page terbaru

--- lib/auth/login_page.dart
+++ lib/auth/login_page.dart
@@ -5,6 +5,10 @@ import 'package:uas/auth/forgot_password_page.dart';
 import 'package:uas/auth/register_page.dart';
 import 'package:uas/startingpage/starting_page.dart';
 import 'package:uas/accountpage/account_page.dart';
+// import 'package:uas/historypage/history_page.dart';
+
+// import 'package:uas/orderfoodpage/choose_zone_page.dart';
+import 'package:uas/orderfoodpage/fauna_zone_page.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
@@ -135,7 +139,10 @@ class _LoginPageState extends State<LoginPage> {
                         width: width * 1,
                         height: height * 0.06,
                         child: ElevatedButton(
-                          onPressed: _login,
+                          // onPressed: _login,
+                          onPressed: () async {
+                            await _login();
+                          },
                           child: Text(
                             'Login',
                             style: TextStyle(
@@ -267,14 +274,25 @@ class _LoginPageState extends State<LoginPage> {
   //               const AccountPage()), // nanti ganti jadi homepage
   //     );
 
+  // _login() async {
+  //   // final user =  //hapus aja
+  //   await _auth.loginUserWithEmailAndPassword(
+  //       _emailController.text, _passwordController.text);
+  //   // boleh dihapus
+  //   // if (user != null) {
+  //   //   // log("User Logged In");
+  //   //   goToHome(context);
+  //   // }
+  // }
+
   _login() async {
-    // final user =  //hapus aja
-    await _auth.loginUserWithEmailAndPassword(
+    final user = await _auth.loginUserWithEmailAndPassword(
         _emailController.text, _passwordController.text);
-    // boleh dihapus
-    // if (user != null) {
-    //   // log("User Logged In");
-    //   goToHome(context);
-    // }
+    if (user != null) {
+      Navigator.push(
+        context,
+        MaterialPageRoute(builder: (context) => const FaunaLandZoneScreen()),
+      );
+    }
   }
 }
