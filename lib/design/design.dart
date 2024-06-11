aaa7f27dd96fe4d0d2b5326f2bcfdf85ccc65941
Jafier Andreas <jafierandreas00@gmail.com>
2024-06-11 15:35:43 +0700

    add navbar, listdata, card-button-widget

---
+++ lib/design/design.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+// Colouring
+Color primaryColor = Color(0xFFFFA62F);
+Color whiteAll = Colors.white;
+
+// TextStyling
+TextStyle appBar = TextStyle(
+  // fontSize: 24,
+  fontWeight: FontWeight.w700,
+);
+
+TextStyle headerText(double size) {
+  return TextStyle(
+    fontSize: size,
+    fontWeight: FontWeight.w500,
+  );
+}
+
+TextStyle labelText = TextStyle(
+  // fontSize: ,
+  fontWeight: FontWeight.w400,
+);
+
+// Jarak SizedBox
+SizedBox h10 = SizedBox(
+  height: 10,
+);
+SizedBox h16 = SizedBox(
+  height: 16,
+);
+SizedBox h20 = SizedBox(
+  height: 20,
+);
