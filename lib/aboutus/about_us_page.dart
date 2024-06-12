d83e7534957a9cd9e7f0288fd16d43cd0b6a61b9
Jessen Chayadi <113822329+Ceee17@users.noreply.github.com>
2024-06-12 18:36:42 +0700

    fix/qr-code

--- lib/aboutus/about_us_page.dart
+++ lib/aboutus/about_us_page.dart
@@ -15,31 +15,31 @@ class _AboutUsPage extends State<AboutUsPage> {
         name: 'Jafier Andreas',
         id: '535220013',
         initials: 'JA',
-        qrCodePath: 'assets/qrmember/linkjafier.png',
+        qrCodePath: 'assets/qrmember/linkJafier.png',
         link: 'Discord : https://s.id/26VYj'),
     Member(
         name: 'Nelson',
         id: '535220021',
         initials: 'NL',
-        qrCodePath: 'assets/qrmember/linknelson.png',
+        qrCodePath: 'assets/qrmember/linkNelson.png',
         link: 'Instagram : https://s.id/26VY9'),
     Member(
         name: 'Jessen Chayadi',
         id: '535220023',
         initials: 'JC',
-        qrCodePath: 'assets/qrmember/linkjc.png',
+        qrCodePath: 'assets/qrmember/linkJC.png',
         link: 'LinkedIn : https://s.id/26VY8'),
     Member(
         name: 'Finnia Li',
         id: '535220030',
         initials: 'FL',
-        qrCodePath: 'assets/qrmember/linkfinnia.png',
+        qrCodePath: 'assets/qrmember/linkFinnia.png',
         link: 'Instagram : https://s.id/26VYm'),
     Member(
         name: 'Timoty Wahyudi P',
         id: '535220043',
         initials: 'TP',
-        qrCodePath: 'assets/qrmember/linktimot.png',
+        qrCodePath: 'assets/qrmember/linkTimot.png',
         link: 'WhatsApp : https://s.id/26VYe'),
   ];
 
