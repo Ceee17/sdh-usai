3d1d87d4070967714e17f2fa93d36b42ec823e02
Jessen Chayadi <113822329+Ceee17@users.noreply.github.com>
2024-06-19 00:48:58 +0700

    finishing server side

--- run.bat
+++ run.bat
@@ -1,4 +1,4 @@
 @echo off
-start cmd /k "cd server && npm run dev"
-start cmd /k "flutter run"
+start cmd /k "cd server && npm install && npm run dev"
+start cmd /k "flutter pub get && flutter run"
 
